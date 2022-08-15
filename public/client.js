8a3eeb4627cfb33e84bcb411b8650e31dc41a343
Chirag Sharma <schirag326@gmail.com>
2022-08-15 14:57:08 +0530

    Update changes

--- public/client.js
+++ public/client.js
@@ -1,54 +1,66 @@
-const socket = io()
-let name;
+const socket = io() // Calling the socket library
+
+let person;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
-do {
-    name = prompt('Please enter your name: ')
-} while(!name)
 
+
+// Getting the name of the user
+// The loop will execute until the user enter's their name  
+do {
+
+    person = prompt('Enter Your name: ')
+
+} while(!person)
+
+
+// Sending message using the 'Enter key'.
 textarea.addEventListener('keyup', (e) => {
     if(e.key === 'Enter') {
         sendMessage(e.target.value)
     }
 })
 
+// Function to send message on pressing the 'Enter key'
 function sendMessage(message) {
     let msg = {
-        user: name,
-        message: message.trim()
+        user: person, // Passing the name that we got from the prompt
+        message: message.trim() // Passing the message that the user has typed
     }
-    // Append 
-    appendMessage(msg, 'outgoing')
-    textarea.value = ''
-    scrollToBottom()
 
-    // Send to server 
-    socket.emit('message', msg)
+// Appending the message so that it firsts display the message on browser and then the message is sent to the server 
+appendMessage(msg, 'outgoing')
+textarea.value = ''
+scrollToBottom();
+
+// Sending to server
+socket.emit('message', msg)
 
 }
 
 function appendMessage(msg, type) {
     let mainDiv = document.createElement('div')
-    let className = type
+    let className = type /// Passing the incoming or outgoing class dynamically
     mainDiv.classList.add(className, 'message')
 
     let markup = `
         <h4>${msg.user}</h4>
         <p>${msg.message}</p>
     `
+
     mainDiv.innerHTML = markup
-    messageArea.appendChild(mainDiv)
+    messageArea.appendChild(mainDiv )
+
 }
 
-// Recieve messages 
+// Receieve messages from the server
 socket.on('message', (msg) => {
     appendMessage(msg, 'incoming')
     scrollToBottom()
 })
 
+
+// This function helps to automatic scroll to the new receieved and sent message
 function scrollToBottom() {
     messageArea.scrollTop = messageArea.scrollHeight
 }
-
-
-
--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
+const  express = require('express') // Importing express
+const app = express() // Calling express
+const http = require('http').createServer(app)  // Creating http server
 
-const PORT = process.env.PORT || 3000
 
+const PORT = process.env.PORT  || 3000 // Assigning port & giving default port as 3000
+
+// Server is listening to the given port
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
 
-app.use(express.static(__dirname + '/public'))
+// Using express middleware for 
+// Creating a route telling the server about the other url
+app.use(express.static(__dirname + '/Public')) 
 
+
+// '/' - It means the default homepage
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    res.sendFile(__dirname + '/index.html') // If this url is requested, then in response we are sending the index.html file
+}) 
 
-// Socket 
-const io = require('socket.io')(http)
 
-io.on('connection', (socket) => {
+// Socket
+const io = require('socket.io')(http) // Socket.io gets to know on which server it has to work on.
+
+io.on('connection', (socket) => { //Server connection is established
     console.log('Connected...')
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
+        socket.broadcast.emit('message', msg)  // Sending the message to all the users who are connected to socket except to the user who is sendin the message    
     })
 
 })
